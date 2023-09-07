e683df0154d6bbc9f5255b48a7c417df600bebaf
Elias Rissanen Uran <elura001@osloskolen.no>
2023-09-07 12:17:51 +0200

    jeg vil dø plz

--- src/pages/api/changeUsername.js
+++ src/pages/api/changeUsername.js
@@ -2,4 +2,12 @@ import connectDB from "./../../../db";
 
 connectDB();
 
-export default async function handler(req, res) {}
+export default async function changeUsername(req, res) {
+  const { newUsername } = req.body;
+
+  try {
+  } catch (error) {
+    console.error("Error while changing username:", error);
+    res.status(500).json({ status: "Internal Server Error" });
+  }
+}
