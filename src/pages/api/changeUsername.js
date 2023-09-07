f10fd2a4919f9a8191da82cf1666e337b93bb060
Iver <ivermoka@icloud.com>
2023-09-07 19:34:31 +0200

    edit profile: added functional team and game + password authentication

--- src/pages/api/changeUsername.js
+++ src/pages/api/changeUsername.js
@@ -4,20 +4,32 @@ import User from "../../../models/User";
 connectDB();
 
 export default async function changeUsername(req, res) {
-  const { newUsername, user } = req.body;
+  const { newUsername, user, password, game, team } = req.body;
 
   try {
     const existingUser = await User.findOne({ username: user });
-
+    if (!existingUser.comparePassword(password)) {
+      return res.status(401).json({ error: "Incorrect password" });
+    }
     if (!existingUser) {
       return res.status(404).json({ error: "User not found" });
     }
+    if (!game) {
+      console.log("Game is null.");
+      return res.status(404).json({ error: "Game not found" });
+    }
+    if (!team) {
+      console.log("Team is null.");
+      return res.status(404).json({ error: "Team not found" });
+    }
 
+    existingUser.team = team;
+    existingUser.game = game;
     existingUser.username = newUsername;
     await existingUser.save();
-    res.status(200).json({ status: "Username changed" });
+    res.status(200).json({ status: "User information changed" });
   } catch (error) {
-    console.error("Error while changing username:", error);
+    console.error("Error while changing user information:", error);
     res.status(500).json({ status: "Internal Server Error" });
   }
 }
