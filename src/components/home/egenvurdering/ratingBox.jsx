8f71c92f77c835302ad225b142e0d5a56bc08d3d
Elias Rissanen Uran <elura001@osloskolen.no>
2023-09-08 08:11:50 +0200

    home page rating system frontend

---
+++ src/components/home/egenvurdering/ratingBox.jsx
@@ -0,0 +1,12 @@
+const RatingBox = ({ number, className, onClick }) => {
+  return (
+    <div
+      onClick={onClick}
+      className={`${className} h-10 w-10 border-2 border-primary rounded-md grid place-items-center font-bold text-xl`}
+    >
+      {number}
+    </div>
+  );
+};
+
+export default RatingBox;
