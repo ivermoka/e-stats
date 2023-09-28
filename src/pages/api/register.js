1882b237219af99fdf8d0232b3777aebea66d326
Iver <ivermoka@icloud.com>
2023-09-28 11:21:35 +0200

    dumt problem fikset

--- src/pages/api/register.js
+++ src/pages/api/register.js
@@ -6,25 +6,27 @@ import jwt from "jsonwebtoken";
 connectDB();
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const { username, password } = req.body;
-    try {
-      const hashedPassword = await bcrypt.hash(password, 10);
+    if (req.method === "POST") {
+        const {username, password, mail} = req.body;
+        try {
+            const hashedPassword = await bcrypt.hash(password, 10);
 
-      const user = new User({ username, password: hashedPassword });
-      await user.save();
-      const token = jwt.sign(
-        { username: username, _id: user._id },
-        process.env.JWT_SECRET,
-        {
-          expiresIn: "1h",
-        },
-      );
-      res.status(201).json({ token: token, username: username });
-    } catch (error) {
-      res.status(500).json({
-        error: "Det finnes allerede en bruker med dette brukernavnet",
-      });
+            const user = new User({mail: mail, username: username, password: hashedPassword});
+            await user.save();
+            const token = jwt.sign(
+                {username: username, _id: user._id},
+                process.env.JWT_SECRET,
+                {
+                    expiresIn: "1h",
+                },
+            );
+            res.status(201).json({token: token, username: username});
+        } catch (error) {
+            res.status(500).json({
+                error: error,
+
+            });
+            console.log(error);
+        }
     }
-  }
 }
