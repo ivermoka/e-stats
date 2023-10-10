89e49fa230ea31bf44426898a850159eabcda638
Elias Rissanen Uran <elura001@osloskolen.no>
2023-10-10 10:10:30 +0200

    light mode på confirm deletes

--- src/components/navbar/dropdown/confirmDelete.jsx
+++ src/components/navbar/dropdown/confirmDelete.jsx
@@ -28,12 +28,12 @@ const ConfirmDelete = ({ setShowConfirmDelete, user }) => {
   return (
     <div
       className={
-        "fixed top-0 left-0 h-screen w-screen bg-bg/70 backdrop-blur-md p-8 flex flex-col text-center gap-4 just-center items-center text-2xl text-text font-semibold"
+        "fixed top-0 left-0 h-screen w-screen bg-bg/70 backdrop-blur-md p-8 flex flex-col text-center gap-4 just-center items-center text-2xl dark:text-text text-textLight font-semibold"
       }
     >
       <div
         className={
-          "rounded-lg bg-primary shadow-md shadow-accent p-4 flex flex-col gap-2"
+          "rounded-lg dark:bg-primary bg-primaryLight shadow-md dark:shadow-accent shadow-accentLight p-4 flex flex-col gap-2"
         }
       >
         <h1>Skriv SLETT for å konfirmere</h1>
@@ -41,15 +41,18 @@ const ConfirmDelete = ({ setShowConfirmDelete, user }) => {
           Du vil ikke kunne få brukeren tilbake
         </h2>
         <input
-          className={"text-bg p-2 rounded-lg shadow-md shadow-accent"}
+          className={
+            "dark:text-bg text-bgLight p-2 rounded-lg shadow-md dark:shadow-accent shadow-accentLight"
+          }
           type="text"
           onChange={(e) => setConfirm(e.target.value)}
         />
         {confirm.toLowerCase() === "slett" ? (
           <button
+            type="button"
             onClick={() => deleteUser()}
             className={
-              "rounded-lg shadow-md shadow-accent bg-bg px-4 py-2 text-red-400 m-4"
+              "rounded-lg shadow-md dark:shadow-accent shadow-accentLight dark:bg-bg bg-bgLight px-4 py-2 text-red-400 m-4"
             }
           >
             Slett bruker
@@ -57,8 +60,11 @@ const ConfirmDelete = ({ setShowConfirmDelete, user }) => {
         ) : null}
       </div>
       <button
+        type="button"
         onClick={() => setShowConfirmDelete(false)}
-        className={"rounded-lg shadow-md shadow-accent bg-primary px-4 py-2"}
+        className={
+          "rounded-lg shadow-md dark:shadow-accent shadow-accentLight dark:bg-primary bg-primaryLight px-4 py-2"
+        }
       >
         Lukk
       </button>
